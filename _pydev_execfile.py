ea0fca0e5330e1532632d300cb7ce839a3d7242b
fabioz <fabiofz@gmail.com>
2014-08-05 20:49:37 -0300

    Minor (missed in merge).

--- _pydev_execfile.py
+++ _pydev_execfile.py
@@ -5,34 +5,14 @@ def execfile(file, glob=None, loc=None):
         glob = sys._getframe().f_back.f_globals
     if loc is None:
         loc = glob
-    stream = open(file, 'rb')
-    try:
-        encoding = None
-        #Get encoding!
-        for _i in range(2):
-            line = stream.readline() #Should not raise an exception even if there are no more contents
-            #Must be a comment line
-            if line.strip().startswith(b'#'):
-                #Don't import re if there's no chance that there's an encoding in the line
-                if b'coding' in line:
-                    import re
-                    p = re.search(br"coding[:=]\s*([-\w.]+)", line)
-                    if p:
-                        try:
-                            encoding = p.group(1).decode('ascii')
-                            break
-                        except:
-                            encoding = None
-    finally:
-        stream.close()
 
-    if encoding:
-        stream = open(file, encoding=encoding)
-    else:
-        stream = open(file)
+    # It seems that the best way is using tokenize.open(): http://code.activestate.com/lists/python-dev/131251/
+    import tokenize
+    stream = tokenize.open(file)
     try:
         contents = stream.read()
     finally:
         stream.close()
-        
-    exec(compile(contents+"\n", file, 'exec'), glob, loc) #execute the script
+
+    #execute the script (note: it's important to compile first to have the filename set in debug mode)
+    exec(compile(contents+"\n", file, 'exec'), glob, loc) 
