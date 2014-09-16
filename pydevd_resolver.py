01602ea607f829e1759bae9d281ba3c43299c3a1
Alexander Marchuk <Alexander.Marchuk@jetbrains.com>
2014-09-16 18:44:54 +0400

    improve ndarray resolver, add container for values

--- pydevd_resolver.py
+++ pydevd_resolver.py
@@ -18,8 +18,7 @@ from pydevd_constants import DictIterItems, xrange
 
 # Note: 300 is already a lot to see in the outline (after that the user should really use the shell to get things)
 # and this also means we'll pass less information to the client side (which makes debugging faster).
-MAX_ITEMS_TO_HANDLE = 300
-MAXIMUM_VARIABLE_REPRESENTATION_SIZE = 1000 
+MAX_ITEMS_TO_HANDLE = 300 
 
 TOO_LARGE_MSG = 'Too large to show contents. Max items to show: ' + str(MAX_ITEMS_TO_HANDLE)
 TOO_LARGE_ATTR = 'Unable to handle:'
@@ -411,18 +410,16 @@ class NdArrayResolver:
         if attribute == 'size':
             return obj.size
         if attribute.startswith('['):
-            items = dict()
-            l = len(obj)
-            format_str = '%0' + str(int(len(str(l)))) + 'd'
+            container = NdArrayItemsContainer()
             i = 0
+            format_str = '%0' + str(int(len(str(len(obj))))) + 'd'
             for item in obj:
-                items[format_str % i] = item
+                setattr(container, format_str % i, item)
                 i += 1
                 if i > MAX_ITEMS_TO_HANDLE:
-                    items.clear()
-                    items[TOO_LARGE_ATTR] = TOO_LARGE_MSG
+                    setattr(container, TOO_LARGE_ATTR, TOO_LARGE_MSG)
                     break
-            return items
+            return container
         return None
 
     def getDictionary(self, obj):
@@ -441,11 +438,10 @@ class NdArrayResolver:
         ret['shape'] = obj.shape
         ret['dtype'] = obj.dtype
         ret['size'] = obj.size
-        if len(str(obj)) > MAXIMUM_VARIABLE_REPRESENTATION_SIZE:
-            ret['[0:%s]' % (len(obj))] = {'items' : str(obj)[0:MAXIMUM_VARIABLE_REPRESENTATION_SIZE]}
-        else:
-            ret['[0:%s]' % (len(obj))] = {'items' : str(obj)}
+        ret['[0:%s] ' % (len(obj))] = list(obj[0:MAX_ITEMS_TO_HANDLE])
         return ret
+
+class NdArrayItemsContainer: pass
 
 
 #=======================================================================================================================
