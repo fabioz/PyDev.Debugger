8142cbfbceb1b80e1e118f7fe133d02da7f1f8bd
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-12-29 15:02:06 +0100

    Matplotlib support in console.

--- pydevconsole.py
+++ pydevconsole.py
@@ -174,6 +174,12 @@ def process_exec_queue(interpreter):
 
     set_return_control_callback(return_control)
 
+    from pydev_import_hook import import_hook_manager
+    from pydev_ipython.matplotlibtools import activate_matplotlib, activate_pylab, activate_pyplot
+    import_hook_manager.add_module_name("matplotlib", activate_matplotlib(interpreter))
+    import_hook_manager.add_module_name("pylab", activate_pylab)
+    import_hook_manager.add_module_name("pyplot", activate_pyplot)
+
     while 1:
         # Running the request may have changed the inputhook in use
         inputhook = get_inputhook()
@@ -330,7 +336,11 @@ def get_interpreter():
     try:
         interpreterInterface = getattr(__builtin__, 'interpreter')
     except AttributeError:
-        interpreterInterface = InterpreterInterface(None, None, threading.currentThread(), show_banner=False)
+        # fake return_controll_callback function just to prevent exception in PyCharm debug console
+        from pydev_ipython.inputhook import set_return_control_callback
+        set_return_control_callback(lambda x: True)
+
+        interpreterInterface = InterpreterInterface(None, None, threading.currentThread())
         setattr(__builtin__, 'interpreter', interpreterInterface)
 
     return interpreterInterface
