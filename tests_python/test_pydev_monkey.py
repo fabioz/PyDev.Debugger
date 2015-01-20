f1d4a61730e07a55ed73be834aa679b8047ec6ca
fabioz <fabiofz@gmail.com>
2015-01-20 08:12:55 -0200

    Improving pydev_monkey test.

--- tests_python/test_pydev_monkey.py
+++ tests_python/test_pydev_monkey.py
@@ -1,6 +1,6 @@
+import sys
 import unittest
 import pydev_monkey
-import sys
 from pydevd import SetupHolder
 from pydev_monkey import pydev_src_dir
 
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
 
     def test_monkey(self):
         original = SetupHolder.setup
-        
+
         try:
             SetupHolder.setup = {'client':'127.0.0.1', 'port': '0'}
             check='''C:\\bin\\python.exe -u -c "
@@ -22,15 +22,85 @@ connect(\\"127.0.0.1\\")
                 'sys.path.append(r\'%s\'); '
                 'import pydevd; pydevd.settrace(host=\'127.0.0.1\', port=0, suspend=False, '
                     'trace_only_current_thread=False, patch_multiprocessing=True); '
-                    '\nconnect(\\"127.0.0.1\\")\n"' % pydev_src_dir, 
+                    '\nconnect(\\"127.0.0.1\\")\n"' % pydev_src_dir,
                 pydev_monkey.patch_arg_str_win(check)
             )
         finally:
             SetupHolder.setup = original
 
     def test_str_to_args_windows(self):
-        
         self.assertEqual(['a', 'b'], pydev_monkey.str_to_args_windows('a "b"'))
-        
+
+    def test_monkey_patch_args_indc(self):
+        original = SetupHolder.setup
+
+        try:
+            SetupHolder.setup = {'client':'127.0.0.1', 'port': '0'}
+            check=['C:\\bin\\python.exe', '-u', '-c', 'connect(\\"127.0.0.1\\")']
+            sys.original_argv = []
+            assert pydev_monkey.patch_args(check) == [
+                'C:\\bin\\python.exe', 
+                '-u', 
+                '-c', 
+                (
+                    'import sys; sys.path.append(r\'%s\'); import pydevd; '
+                    'pydevd.settrace(host=\'127.0.0.1\', port=0, suspend=False, trace_only_current_thread=False, patch_multiprocessing=True); '
+                    'connect(\\"127.0.0.1\\")'
+                ) % pydev_src_dir 
+            ]
+        finally:
+            SetupHolder.setup = original
+            
+    def test_monkey_patch_args_no_indc(self):
+        original = SetupHolder.setup
+
+        try:
+            SetupHolder.setup = {'client':'127.0.0.1', 'port': '0'}
+            check=['C:\\bin\\python.exe', 'connect(\\"127.0.0.1\\")']
+            sys.original_argv = ['my', 'original', 'argv']
+            if sys.platform == 'win32':
+                assert pydev_monkey.patch_args(check) == [
+                    'C:\\bin\\python.exe', '"my"', '"original"', '"argv"', 'connect(\\"127.0.0.1\\")']
+            else:
+                assert pydev_monkey.patch_args(check) == [
+                    'C:\\bin\\python.exe', 'my', 'original', 'argv', 'connect(\\"127.0.0.1\\")']
+        finally:
+            SetupHolder.setup = original
+            
+    def test_monkey_patch_args_no_indc_with_pydevd(self):
+        original = SetupHolder.setup
+
+        try:
+            SetupHolder.setup = {'client':'127.0.0.1', 'port': '0'}
+            check=['C:\\bin\\python.exe', 'pydevd.py', 'connect(\\"127.0.0.1\\")', 'bar']
+            sys.original_argv = ['my', 'original', 'argv']
+            
+            assert pydev_monkey.patch_args(check) == [
+                'C:\\bin\\python.exe', 'pydevd.py', 'connect(\\"127.0.0.1\\")', 'bar']
+        finally:
+            SetupHolder.setup = original
+            
+    def test_monkey_patch_args_no_indc_without_pydevd(self):
+        original = SetupHolder.setup
+
+        try:
+            SetupHolder.setup = {'client':'127.0.0.1', 'port': '0'}
+            check=['C:\\bin\\python.exe', 'target.py', 'connect(\\"127.0.0.1\\")', 'bar']
+            sys.original_argv = ['pydevd.py', '--a=1', 'b', '--c=2', '--file', 'ignore_this.py']
+            
+            assert pydev_monkey.patch_args(check) == [
+                'C:\\bin\\python.exe', 
+                '"pydevd.py"', 
+                '"--a=1"', 
+                '"b"', 
+                '"--c=2"', 
+                '"--file"', 
+                'target.py', 
+                'connect(\\"127.0.0.1\\")', 
+                'bar', 
+            ]
+        finally:
+            SetupHolder.setup = original
+
 if __name__ == '__main__':
     unittest.main()
