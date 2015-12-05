35eb248985773c05786c9190824597845420138e
Fabio Zadrozny <fabioz@esss.com.br>
2015-12-05 09:45:40 -0200

    Updating script for renames.

--- build/rename_pep8.py
+++ build/rename_pep8.py
@@ -1,105 +1,71 @@
 import re
+import os
+import names_to_rename
+
 _CAMEL_RE = re.compile(r'(?<=[a-z])([A-Z])')
+_CAMEL_DEF_RE = re.compile(r'(def )((([A-Z0-9]+|[a-z0-9])[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*)')
 
 def _normalize(name):
     return _CAMEL_RE.sub(lambda x: '_' + x.group(1).lower(), name).lower()
 
-def main():
-    import os
-
-    # traverse root directory, and list directories as dirs and files as files
-    for root, dirs, files in os.walk(os.path.dirname(os.path.dirname(__file__))):
+def find_matches_in_contents(contents):
+    return [x[1] for x in re.findall(_CAMEL_DEF_RE, contents)]
+    
+def iter_files_in_dir(dirname):
+    for root, _dirs, files in os.walk(dirname):
         for filename in files:
-            if filename.endswith('.py') and filename != 'rename_pep8.py':
+            if filename.endswith('.py') and filename not in ('rename_pep8.py', 'names_to_rename.py'):
                 path = os.path.join(root, filename)
                 with open(path, 'rb') as stream:
                     initial_contents = stream.read()
-                for key, val in name_to_new_val.iteritems():
-                    contents = re.sub(key, val, initial_contents)
 
-                if contents != initial_contents:
-                    if re.findall(r'\b%s\b' % (val,), initial_contents):
-                        raise AssertionError('Error in:\n%s\n%s is already being used (and changes may conflict).' % (path, val,))
-                    
-                with open(path, 'wb') as stream:
-                    stream.write(contents)
+                yield path, initial_contents
+            
+def find_matches():
+    found = set()
+    for path, initial_contents in iter_files_in_dir(os.path.dirname(os.path.dirname(__file__))):
+        found.update(find_matches_in_contents(initial_contents))
+    print '\n'.join(sorted(found))
+    print 'Total', len(found)
+    
+def make_replace():
+    re_name_to_new_val = load_re_to_new_val()
+    # traverse root directory, and list directories as dirs and files as files
+    for path, initial_contents in iter_files_in_dir(os.path.dirname(os.path.dirname(__file__))):
+        for key, val in re_name_to_new_val.iteritems():
+            contents = re.sub(key, val, initial_contents)
 
-
-_NAMES = '''
-sendCaughtExceptionStack
-sendBreakpointConditionException
-setSuspend
-processThreadNotAlive
-sendCaughtExceptionStackProceeded
-doWaitSuspend
-SetTraceForFrameAndParents
-prepareToRun
-processCommandLine
-initStdoutRedirect
-initStderrRedirect
-OnRun
-doKillPydevThread
-stopTrace
-handleExcept
-processCommand
-processNetCommand
-addCommand
-StartClient
+        if contents != initial_contents:
+            if re.findall(r'\b%s\b' % (val,), initial_contents):
+                raise AssertionError('Error in:\n%s\n%s is already being used (and changes may conflict).' % (path, val,))
+            
+        with open(path, 'wb') as stream:
+            stream.write(contents)
 
 
-getNextSeq
-makeMessage
-StartServer
+def load_re_to_new_val():
+    name_to_new_val = {}
+    for n in names_to_rename.NAMES.splitlines():
+        n = n.strip()
+        if not n.startswith('#') and n:
+            name_to_new_val[r'\b'+n+r'\b'] = _normalize(n)
+    return name_to_new_val
 
-threadToXML
-makeErrorMessage
-
-
-makeThreadCreatedMessage
-makeCustomFrameCreatedMessage
-makeListThreadsMessage
-makeVariableChangedMessage
-makeIoMessage    
-
-
-makeVersionMessage
-makeThreadKilledMessage
-makeThreadSuspendStr
-makeValidXmlValue
-makeThreadSuspendMessage
-makeThreadRunMessage
-makeGetVariableMessage
-makeGetArrayMessage
-makeGetFrameMessage
-makeEvaluateExpressionMessage
-makeGetCompletionsMessage
-makeGetFileContents
-makeSendBreakpointExceptionMessage
-makeSendCurrExceptionTraceMessage
-makeSendCurrExceptionTraceProceededMessage
-makeSendConsoleMessage
-makeCustomOperationMessage
-makeLoadSourceMessage
-makeShowConsoleMessage
-
-
-makeExitMessage
-canBeExecutedBy
-doIt
-additionalInfo
-cmdFactory
-GetExceptionTracebackStr
-_GetStackStr
-_InternalSetTrace
-ReplaceSysSetTraceFunc
-RestoreSysSetTraceFunc
-'''
-name_to_new_val = {}
-for n in _NAMES.splitlines():
-    n = n.strip()
-    if not n.startswith('#') and n:
-        name_to_new_val[r'\b'+n+r'\b'] = _normalize(n)
-
+def test():
+    assert _normalize('RestoreSysSetTraceFunc') == 'restore_sys_set_trace_func'
+    assert _normalize('restoreSysSetTraceFunc') == 'restore_sys_set_trace_func'
+    assert _normalize('Restore') == 'restore'
+    matches = find_matches_in_contents('''
+    def CamelCase()
+    def camelCase()
+    def ignore()
+    def ignore_this()
+    def Camel()
+    def CamelCaseAnother()
+    ''')
+    assert matches == ['CamelCase', 'camelCase', 'Camel', 'CamelCaseAnother']
+    
 if __name__ == '__main__':
-    main()
+    #find_matches()
+    make_replace()
 
