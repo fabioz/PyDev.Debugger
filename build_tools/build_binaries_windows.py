194f2493b0bf9cccec6c175d63410479c1a4604b
Kiryl Chetyrbak <kirylchetyrbak@gmail.com>
2016-12-09 16:41:52 -0500

    Added Python 2.6 supporting

--- build_tools/build_binaries_windows.py
+++ build_tools/build_binaries_windows.py
@@ -9,6 +9,13 @@ Creating the needed environments for creating the pre-compiled distribution on W
 
 Create the environments:
 
+C:\tools\Miniconda32\Scripts\conda create -y -f -n py26_32 python=2.6 cython numpy nose ipython pip
+C:\tools\Miniconda32\Scripts\activate py26_32
+pip install "django>=1.7,<1.8"
+pip install -U "setuptools>=0.9"
+pip install -U "pip>=1.4" "wheel>=0.21" twine
+deactivate
+
 C:\tools\Miniconda32\Scripts\conda create -y -f -n py27_32 python=2.7 cython numpy nose ipython pip
 C:\tools\Miniconda32\Scripts\activate py27_32
 pip install "django>=1.7,<1.8"
@@ -26,6 +33,13 @@ deactivate
 C:\tools\Miniconda32\Scripts\conda create -y -f -n py35_32 python=3.5 cython numpy nose ipython pip
 C:\tools\Miniconda32\Scripts\activate py35_32
 pip install "django>=1.9"
+pip install -U "setuptools>=0.9"
+pip install -U "pip>=1.4" "wheel>=0.21" twine
+deactivate
+
+C:\tools\Miniconda\Scripts\conda create -y -f -n py26_64 python=2.6 cython numpy nose ipython pip
+C:\tools\Miniconda\Scripts\activate py26_64
+pip install "django>=1.7,<1.8"
 pip install -U "setuptools>=0.9"
 pip install -U "pip>=1.4" "wheel>=0.21" twine
 deactivate
@@ -63,13 +77,15 @@ miniconda32_envs = os.getenv('MINICONDA32_ENVS', r'C:\tools\Miniconda32\envs')
 miniconda64_envs = os.getenv('MINICONDA64_ENVS', r'C:\tools\Miniconda\envs')
 
 python_installations = [
+    r'%s\py26_32\python.exe' % miniconda32_envs,
     r'%s\py27_32\python.exe' % miniconda32_envs,
-    r'%s\py34_32\python.exe' % miniconda32_envs,
-    r'%s\py35_32\python.exe' % miniconda32_envs,
-
-    r'%s\py27_64\python.exe' % miniconda64_envs,
-    r'%s\py34_64\python.exe' % miniconda64_envs,
-    r'%s\py35_64\python.exe' % miniconda64_envs,
+    # r'%s\py34_32\python.exe' % miniconda32_envs,
+    # r'%s\py35_32\python.exe' % miniconda32_envs,
+    #
+    # r'%s\py26_64\python.exe' % miniconda64_envs,
+    # r'%s\py27_64\python.exe' % miniconda64_envs,
+    # r'%s\py34_64\python.exe' % miniconda64_envs,
+    # r'%s\py35_64\python.exe' % miniconda64_envs,
 ]
 
 root_dir = os.path.dirname(os.path.dirname(__file__))
