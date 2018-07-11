f18ba4143869f92f41ae3af20c282d209f568e67
fabioz <fabiofz@gmail.com>
2018-07-11 14:25:46 -0300

    Minor

--- _pydevd_bundle/pydevd_utils.py
+++ _pydevd_bundle/pydevd_utils.py
@@ -204,7 +204,7 @@ def _get_default_library_roots():
 
     if hasattr(site, 'getusersitepackages'):
         site_paths = site.getusersitepackages()
-        if isinstance(site_paths, list):
+        if isinstance(site_paths, (list, tuple)):
             for site_path in site_paths:
                 roots.append(site_path)
         else:
@@ -212,7 +212,7 @@ def _get_default_library_roots():
 
     if hasattr(site, 'getsitepackages'):
         site_paths = site.getsitepackages()
-        if isinstance(site_paths, list):
+        if isinstance(site_paths, (list, tuple)):
             for site_path in site_paths:
                 roots.append(site_path)
         else:
@@ -249,7 +249,6 @@ def in_project_roots(filename, filename_to_in_scope_cache=_FILENAME_TO_IN_SCOPE_
         original_filename = filename
         if not filename.startswith('<'):
             filename = _normpath(filename)
-        filename = _normpath(filename)
         
         found_in_project = []
         for root in project_roots:
@@ -267,7 +266,7 @@ def in_project_roots(filename, filename_to_in_scope_cache=_FILENAME_TO_IN_SCOPE_
             if not found_in_library:
                 in_project = True
             else:
-                # found in both, let's see which one has the higher path
+                # Found in both, let's see which one has the bigger path matched.
                 if max(len(x) for x in found_in_project) > max(len(x) for x in found_in_library):
                     in_project = True
                     
