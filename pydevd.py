5784c2dc2d75c29383825879f93fc616f3359035
fabioz <fabiofz@gmail.com>
2018-08-16 10:55:41 -0300

    Add support for breakpoint() builtin.

--- pydevd.py
+++ pydevd.py
@@ -43,7 +43,7 @@ from _pydevd_frame_eval.pydevd_frame_eval_main import frame_eval_func, stop_fram
 from _pydevd_bundle.pydevd_utils import save_main_module
 from pydevd_concurrency_analyser.pydevd_concurrency_logger import ThreadingLogger, AsyncioLogger, send_message, cur_time
 from pydevd_concurrency_analyser.pydevd_thread_wrappers import wrap_threads
-from pydevd_file_utils import get_fullname, rPath
+from pydevd_file_utils import get_fullname, rPath, get_package_dir
 import pydev_ipython  # @UnusedImport
 
 __version_info__ = (1, 3, 3)
@@ -55,11 +55,37 @@ __version__ = '.'.join(__version_info_str__)
 
 #IMPORTANT: pydevd_constants must be the 1st thing defined because it'll keep a reference to the original sys._getframe
 
+def install_breakpointhook(pydevd_breakpointhook=None):
+    if pydevd_breakpointhook is None:
+        def pydevd_breakpointhook(*args, **kwargs):
+            hookname = os.getenv('PYTHONBREAKPOINT')
+            if hookname is not None and len(hookname) > 0 and hasattr(sys, '__breakpointhook__'):
+                sys.__breakpointhook__(*args, **kwargs)
+            else:
+                settrace(*args, **kwargs)
 
+    if sys.version_info[0:2] >= (3, 7):
+        # There are some choices on how to provide the breakpoint hook. Namely, we can provide a 
+        # PYTHONBREAKPOINT which provides the import path for a method to be executed or we
+        # can override sys.breakpointhook.
+        # pydevd overrides sys.breakpointhook instead of providing an environment variable because
+        # it's possible that the debugger starts the user program but is not available in the 
+        # PYTHONPATH (and would thus fail to be imported if PYTHONBREAKPOINT was set to pydevd.settrace).
+        # Note that the implementation still takes PYTHONBREAKPOINT in account (so, if it was provided
+        # by someone else, it'd still work).
+        sys.breakpointhook = pydevd_breakpointhook
+    else:
+        if sys.version_info[0] >= 3:
+            import builtins as __builtin__ # Py3
+        else:
+            import __builtin__
 
+        # In older versions, breakpoint() isn't really available, so, install the hook directly
+        # in the builtins.
+        __builtin__.breakpoint = pydevd_breakpointhook
 
-
-
+# Install the breakpoint hook at import time.
+install_breakpointhook()
 
 SUPPORT_PLUGINS = not IS_JYTH_LESS25
 PluginManager = None
