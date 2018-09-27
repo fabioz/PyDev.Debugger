10e3aa9b8fa8c6c10b2322b6a9e2fba893e964d7
fabioz <fabiofz@gmail.com>
2018-09-27 15:49:21 -0300

    Fix for Python 3.4

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ args = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Debuggers',
     ],
     entry_points={
