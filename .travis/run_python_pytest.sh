8ca1350c04a91e6bab663fe265d44e161b90ef53
Fabio Zadrozny <fabiofz@gmail.com>
2020-08-20 08:10:24 -0300

    wip

--- .travis/run_python_pytest.sh
+++ .travis/run_python_pytest.sh
@@ -4,9 +4,9 @@ fi
 
 if [[ ("$PYDEVD_PYTHON_VERSION" == "2.6" || "$PYDEVD_PYTHON_VERSION" == "2.7") ]]; then
   # pytest-xdist not available for python == 2.6 and timing out without output with 2.7
-    python -m pytest
+    python -m pytest -k test_case_referrers
 
 else
-    python -m pytest -n auto
+    python -m pytest -n auto -k test_case_referrers
 
 fi
