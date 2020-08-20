b25dfb5be1575b9d11fc79e48a5df0682a668e08
Fabio Zadrozny <fabiofz@gmail.com>
2020-08-20 08:13:57 -0300

    wip

--- .travis/run_python_pytest.sh
+++ .travis/run_python_pytest.sh
@@ -4,10 +4,10 @@ fi
 
 if [[ ("$PYDEVD_PYTHON_VERSION" == "2.6" || "$PYDEVD_PYTHON_VERSION" == "2.7") ]]; then
   # pytest-xdist not available for python == 2.6 and timing out without output with 2.7
-    python -m pytest -rf --ignore=_pydevd_frame_eval/vendored
+    python -m pytest -rf --ignore=_pydevd_frame_eval/vendored -k test_case_referrers
 
 else
     # Note: run vendored tests for bytecode in Python 3.
-    python -m pytest -n auto -rf
+    python -m pytest -n auto -rf -k test_case_referrers
 
 fi
