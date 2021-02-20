fabioz/PyDev.Debugger

commit b92dca66a86a505a8e8b2d08898c50635e707d87

Fixed 'NoneType' object is not callable (ptvsd: 987).

_pydevd_bundle/pydevd_stackless.py
@@ -4,10 +4,10 @@ import weakref
 import sys
 
 from _pydevd_bundle.pydevd_comm import get_global_debugger
-from _pydevd_bundle.pydevd_constants import threading, call_only_once
+from _pydevd_bundle.pydevd_constants import call_only_once
+from _pydev_imps._pydev_saved_modules import threading
 from _pydevd_bundle.pydevd_constants import dict_items
 from _pydevd_bundle.pydevd_custom_frames import update_custom_frame, remove_custom_frame, add_custom_frame
-from _pydevd_bundle.pydevd_dont_trace_files import DONT_TRACE
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame
 import stackless  # @UnresolvedImport
 
@@ -218,10 +218,9 @@ def _schedule_callback(prev, next):
                         if frame is current_frame:
                             frame = frame.f_back
                         if frame is not None:
-                            base = get_abs_path_real_path_and_base_from_frame(frame)[-1]
+                            abs_real_path_and_base = get_abs_path_real_path_and_base_from_frame(frame)
                             # print >>sys.stderr, "SchedCB: %r, %d, '%s', '%s'" % (tasklet, frame.f_lineno, _filename, base)
-                            is_file_to_ignore = base in DONT_TRACE
-                            if not is_file_to_ignore:
+                            if debugger.get_file_type(abs_real_path_and_base) is None:
                                 tasklet_info.update_name()
                                 if tasklet_info.frame_id is None:
                                     tasklet_info.frame_id = add_custom_frame(frame, tasklet_info.tasklet_name, tasklet.thread_id)
@@ -286,9 +285,8 @@ if not hasattr(stackless.tasklet, "trace_function"):
                         if tasklet.paused or tasklet.blocked or tasklet.scheduled:
                             if tasklet.frame and tasklet.frame.f_back:
                                 f_back = tasklet.frame.f_back
-                                base = get_abs_path_real_path_and_base_from_frame(f_back)[-1]
-                                is_file_to_ignore = base in DONT_TRACE
-                                if not is_file_to_ignore:
+                                abs_real_path_and_base = get_abs_path_real_path_and_base_from_frame(f_back)
+                                if debugger.get_file_type(abs_real_path_and_base) is None:
                                     if tasklet_info.frame_id is None:
                                         tasklet_info.frame_id = add_custom_frame(f_back, tasklet_info.tasklet_name, tasklet.thread_id)
                                     else:

_pydevd_bundle/pydevd_additional_thread_info_regular.py
@@ -82,6 +82,9 @@ class PyDBAdditionalThreadInfo(object):
         'pydev_func_name',
         'suspended_at_unhandled',
         'trace_suspend_type',
+        'top_level_thread_tracer_no_back_frames',
+        'top_level_thread_tracer_unhandled',
+        'thread_tracer',
     ]
     # ENDIF
 
@@ -102,6 +105,9 @@ class PyDBAdditionalThreadInfo(object):
         self.pydev_func_name = '.invalid.'  # Must match the type in cython
         self.suspended_at_unhandled = False
         self.trace_suspend_type = 'trace'  # 'trace' or 'frame_eval'
+        self.top_level_thread_tracer_no_back_frames = []
+        self.top_level_thread_tracer_unhandled = None
+        self.thread_tracer = None
 
     def get_topmost_frame(self, thread):
         '''

_pydevd_bundle/pydevd_breakpoints.py
@@ -75,20 +75,11 @@ class LineBreakpoint(object):
         return ret
 
 
-def get_exception_full_qname(exctype):
-    if not exctype:
-        return None
-    return str(exctype.__module__) + '.' + exctype.__name__
-
-
-def get_exception_name(exctype):
-    if not exctype:
-        return None
-    return exctype.__name__
-
-
 def get_exception_breakpoint(exctype, exceptions):
-    exception_full_qname = get_exception_full_qname(exctype)
+    if not exctype:
+        exception_full_qname = None
+    else:
+        exception_full_qname = str(exctype.__module__) + '.' + exctype.__name__
 
     exc = None
     if exceptions is not None:
@@ -103,11 +94,10 @@ def get_exception_breakpoint(exctype, exceptions):
 
 
 def stop_on_unhandled_exception(py_db, thread, additional_info, arg):
-    from _pydevd_bundle.pydevd_frame import handle_breakpoint_condition, handle_breakpoint_expression
     exctype, value, tb = arg
     break_on_uncaught_exceptions = py_db.break_on_uncaught_exceptions
     if break_on_uncaught_exceptions:
-        exception_breakpoint = get_exception_breakpoint(exctype, break_on_uncaught_exceptions)
+        exception_breakpoint = py_db.get_exception_breakpoint(exctype, break_on_uncaught_exceptions)
     else:
         exception_breakpoint = None
 
@@ -137,12 +127,12 @@ def stop_on_unhandled_exception(py_db, thread, additional_info, arg):
         frame = frames[-1]
     add_exception_to_frame(frame, arg)
     if exception_breakpoint.condition is not None:
-        eval_result = handle_breakpoint_condition(py_db, additional_info, exception_breakpoint, frame)
+        eval_result = py_db.handle_breakpoint_condition(additional_info, exception_breakpoint, frame)
         if not eval_result:
             return
 
     if exception_breakpoint.expression is not None:
-        handle_breakpoint_expression(exception_breakpoint, additional_info, frame)
+        py_db.handle_breakpoint_expression(exception_breakpoint, additional_info, frame)
 
     try:
         additional_info.pydev_message = exception_breakpoint.qname
@@ -151,7 +141,7 @@ def stop_on_unhandled_exception(py_db, thread, additional_info, arg):
 
     pydev_log.debug('Handling post-mortem stop on exception breakpoint %s' % (exception_breakpoint.qname,))
 
-    py_db.stop_on_unhandled_exception(thread, frame, frames_byid, arg)
+    py_db.do_stop_on_unhandled_exception(thread, frame, frames_byid, arg)
 
 
 def get_exception_class(kls):

_pydevd_bundle/pydevd_comm.py
@@ -104,8 +104,6 @@ except:
     except:
         import io as StringIO
 
-from _pydevd_bundle.pydevd_dont_trace_files import DONT_TRACE
-get_file_type = DONT_TRACE.get
 
 # CMD_XXX constants imported for backward compatibility
 from _pydevd_bundle.pydevd_comm_constants import *  # @UnusedWildImport

_pydevd_bundle/pydevd_constants.py
@@ -236,15 +236,50 @@ try:
 except:
     from io import StringIO
 
-NO_FTRACE = None
-
-if sys.version_info[:2] in ((2, 6), (3, 3), (3, 4)):
+if IS_JYTHON:
 
     def NO_FTRACE(frame, event, arg):
-        # In Python <= 2.6 and <= 3.4, if we're tracing a method, frame.f_trace may not be set
-        # to None, it must always be set to a tracing function.
-        # See: tests_python.test_tracing_gotchas.test_tracing_gotchas
         return None
+
+else:
+    _curr_trace = sys.gettrace()
+
+    # Set a temporary trace which does nothing for us to test (otherwise setting frame.f_trace has no
+    # effect).
+    def _temp_trace(frame, event, arg):
+        return None
+
+    sys.settrace(_temp_trace)
+
+    def _check_ftrace_set_none():
+        '''
+        Will throw an error when executing a line event
+        '''
+        sys._getframe().f_trace = None
+        _line_event = 1
+        _line_event = 2
+
+    try:
+        _check_ftrace_set_none()
+
+        def NO_FTRACE(frame, event, arg):
+            frame.f_trace = None
+            return None
+
+    except TypeError:
+
+        def NO_FTRACE(frame, event, arg):
+            # In Python <= 2.6 and <= 3.4, if we're tracing a method, frame.f_trace may not be set
+            # to None, it must always be set to a tracing function.
+            # See: tests_python.test_tracing_gotchas.test_tracing_gotchas
+            #
+            # Note: Python 2.7 sometimes works and sometimes it doesn't depending on the minor
+            # version because of https://bugs.python.org/issue20041 (although bug reports didn't
+            # include the minor version, so, mark for any Python 2.7 as I'm not completely sure
+            # the fix in later 2.7 versions is the same one we're dealing with).
+            return None
+
+    sys.settrace(None)
 
 
 #=======================================================================================================================

_pydevd_bundle/pydevd_cython.pxd
@@ -15,3 +15,6 @@ cdef class PyDBAdditionalThreadInfo:
     cdef public str pydev_func_name;
     cdef public bint suspended_at_unhandled;
     cdef public str trace_suspend_type;
+    cdef public object top_level_thread_tracer_no_back_frames;
+    cdef public object top_level_thread_tracer_unhandled;
+    cdef public object thread_tracer;

commit f19985cab2cddd37f4cce2ee41e33b77bf57795e

Fix a regression that broke debugging Stackless tasklets
Commit b92dca6 introduced a new local variable "debugger", but didn't
initialise it.

_pydevd_bundle/pydevd_stackless.py
@@ -222,12 +222,14 @@ def _schedule_callback(prev, next):
                             frame = frame.f_back
                         if frame is not None:
                             # print >>sys.stderr, "SchedCB: %r, %d, '%s', '%s'" % (tasklet, frame.f_lineno, _filename, base)
-                            if debugger.get_file_type(frame) is None:
+                            debugger = get_global_debugger()
+                            if debugger is not None and debugger.get_file_type(frame) is None:
                                 tasklet_info.update_name()
                                 if tasklet_info.frame_id is None:
                                     tasklet_info.frame_id = add_custom_frame(frame, tasklet_info.tasklet_name, tasklet.thread_id)
                                 else:
                                     update_custom_frame(tasklet_info.frame_id, frame, tasklet.thread_id, name=tasklet_info.tasklet_name)
+                            debugger = None
 
                     elif tasklet is next or is_running:
                         if tasklet_info.frame_id is not None:
@@ -288,11 +290,13 @@ if not hasattr(stackless.tasklet, "trace_function"):
                         if tasklet.paused or tasklet.blocked or tasklet.scheduled:
                             if tasklet.frame and tasklet.frame.f_back:
                                 f_back = tasklet.frame.f_back
-                                if debugger.get_file_type(f_back) is None:
+                                debugger = get_global_debugger()
+                                if debugger is not None and debugger.get_file_type(f_back) is None:
                                     if tasklet_info.frame_id is None:
                                         tasklet_info.frame_id = add_custom_frame(f_back, tasklet_info.tasklet_name, tasklet.thread_id)
                                     else:
                                         update_custom_frame(tasklet_info.frame_id, f_back, tasklet.thread_id)
+                                debugger = None
 
                         elif tasklet.is_current:
                             if tasklet_info.frame_id is not None: