7946ed6209923b6fb71853b034a4ec8aa51244a7
Fabio Zadrozny <fabiofz@gmail.com>
2022-12-29 16:19:44 -0300

    Update to 2.9.5 and add long_description (just having a README.rst isn't enough for setuptools anymore).

--- setup.py
+++ setup.py
@@ -53,10 +53,15 @@ for root, dirs, files in os.walk("pydevd_attach_to_process"):
 import pydevd
 version = pydevd.__version__
 
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.rst'), 'r', encoding='utf-8') as stream:
+    long_description = stream.read()
+
 args = dict(
     name='pydevd',
     version=version,
     description='PyDev.Debugger (used in PyDev, PyCharm and VSCode Python)',
+    long_description_content_type='text/x-rst',
+    long_description=long_description,
     author='Fabio Zadrozny and others',
     url='https://github.com/fabioz/PyDev.Debugger/',
     license='EPL, Apache 2.0',
