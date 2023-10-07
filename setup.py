f26be3244f422b173562311eadbb423d2efa3856
Fabio Zadrozny <fabiofz@gmail.com>
2023-10-07 10:32:44 -0300

    Release pydevd to 2.10.0 (initial support for Python 3.12).

--- setup.py
+++ setup.py
@@ -119,6 +119,7 @@ args = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Debuggers',
     ],
     entry_points={
