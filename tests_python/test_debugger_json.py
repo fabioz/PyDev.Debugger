83f9bc54a9c7de2db4d3706bbfc18daaa79ef069
Rich Chiodo false <rchiodo@microsoft.com>
2024-10-11 16:30:39 -0700

    Skip new test on pypy

--- tests_python/test_debugger_json.py
+++ tests_python/test_debugger_json.py
@@ -1369,7 +1369,7 @@ def test_case_sys_exit_0_handled_exception(case_setup_dap, break_on_system_exit_
 
 
 @pytest.mark.skipif(
-    sys.platform == "darwin" or not SUPPORT_ATTACH_TO_PID,
+    sys.platform == "darwin" or not SUPPORT_ATTACH_TO_PID or IS_PYPY,
     reason="https://github.com/microsoft/ptvsd/issues/1988",
 )
 @pytest.mark.parametrize("raised", ["raised", ""])
