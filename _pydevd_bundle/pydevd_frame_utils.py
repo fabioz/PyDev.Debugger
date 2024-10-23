7a77191d70bfda76a5cf0e800a247fceb5d35881
Rich Chiodo false <rchiodo@microsoft.com>
2024-10-23 09:38:22 -0700

    Fix linter

--- _pydevd_bundle/pydevd_frame_utils.py
+++ _pydevd_bundle/pydevd_frame_utils.py
@@ -1,4 +1,4 @@
-from _pydevd_bundle.pydevd_constants import EXCEPTION_TYPE_USER_UNHANDLED, EXCEPTION_TYPE_UNHANDLED, IS_PY311_OR_GREATER, IS_PY313_OR_GREATER
+from _pydevd_bundle.pydevd_constants import EXCEPTION_TYPE_USER_UNHANDLED, EXCEPTION_TYPE_UNHANDLED, IS_PY311_OR_GREATER
 from _pydev_bundle import pydev_log
 import itertools
 from typing import Any, Dict
